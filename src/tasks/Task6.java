138237873840b5b0c2ca1ad94f9ee60dccc07b47
mpv <pavel.meld@gmail.com>
2019-11-26 10:18:09 +0300

    Finished Task 6

--- src/tasks/Task6.java
+++ src/tasks/Task6.java
@@ -7,6 +7,7 @@ import common.Task;
 import java.time.Instant;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -23,7 +24,20 @@ public class Task6 implements Task {
   private Set<String> getPersonDescriptions(Collection<Person> persons,
                                             Map<Integer, Set<Integer>> personAreaIds,
                                             Collection<Area> areas) {
-    return new HashSet<>();
+    HashSet<String> output = new HashSet<>();
+    Map<Integer,String> areaHelper = new HashMap<>();
+
+    areas.forEach( a -> areaHelper.put(a.getId(), a.getName()));
+
+    persons.forEach(
+      (p) -> {
+        personAreaIds.get(p.getId()).forEach((a) -> {
+            output.add(p.getFirstName() + " - " + areaHelper.get(a));
+        });
+      }
+    );
+
+    return output;
   }
 
   @Override
